87ad6e353c28d7cfdb37bb87a2cb3953e67d801d
Washington Luis <washingtonluismm@gmail.com>
2021-11-20 14:57:56 -0800

    after workshop 3

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderCampsite({campsite}){
     return(
@@ -7,12 +8,11 @@ function RenderCampsite({campsite}){
             <Card>
                 <CardImg top src = { campsite.image } alt = { campsite.name } />
                 <CardBody>
-                    <CardTitle> { campsite.name } </CardTitle>
-                    <CardText> { campsite.description } </CardText>
+                    <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
         </div>
-    )
+    );
 }
 
 function RenderComments({comments}){
@@ -34,15 +34,25 @@ function RenderComments({comments}){
         <div />        
 }
 
-function CampsiteInfo ({props}) {
-    if (props.campsite){
+function CampsiteInfo ({campsites, comments}) {
+    if (campsites){
         return (
             <div className="container">
                 <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{campsites.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{campsites.name}</h2>
+                        <hr />
+                    </div>
                 </div>
-            </div>  
+                <div className="row">
+                    <RenderCampsite campsite={campsites} />
+                    <RenderComments comments={comments} />
+                </div>
+            </div>
         );
     } 
     return <div />;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
@@ -23,8 +25,6 @@ class Main extends Component {
             promotions: PROMOTIONS
         };
     }
-
-    
 
     render() {
 
@@ -53,6 +53,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
