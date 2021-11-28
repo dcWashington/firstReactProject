85ff518085b39ccb477fc457ea05d5b15f2bb00b
Washington Luis <washingtonluismm@gmail.com>
2021-11-27 23:09:20 -0800

    ws tsk 1 and 2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,93 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, 
+    Button, Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
+
+class CommentForm extends Component{
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false,
+            // rating: '1',
+            // author: '',
+            // text: ''
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(value) {
+        console.log("Current state is: " + JSON.stringify(value));
+        alert("Current state is: " + JSON.stringify(value));
+        this.toggleModal();
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal}>
+                <i className="fa fa-pencil fa-lg" /> Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select 
+                                    model=".rating" 
+                                    value={this.state.rating}
+                                    onChange={this.handleInputChange}
+                                    name="rating" 
+                                    id="rating"
+                                    className="form-control">
+                                        <option>1</option>                                    
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text 
+                                    model=".author" 
+                                    placeholder="Your Name"
+                                    value={this.state.author}
+                                    onChange={this.handleInputChange}
+                                    id="author" 
+                                    name="author"  
+                                    className="form-control" 
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">Comments</Label>
+                                <Control.textarea 
+                                    model=".text" 
+                                    value={this.state.text}
+                                    onChange={this.handleInputChange}
+                                    id="text" 
+                                    name="text"
+                                    className="form-control"
+                                    rows="6"
+                                />
+                            </div>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment> 
+        )
+    }
+}
 
 function RenderCampsite({campsite}){
     return(
@@ -28,6 +115,8 @@ function RenderComments({comments}){
                         </div>
                     );
                 })}
+                <CommentForm />
+
             </div>
         );
     } return
@@ -51,6 +140,7 @@ function CampsiteInfo ({campsites, comments}) {
                 <div className="row">
                     <RenderCampsite campsite={campsites} />
                     <RenderComments comments={comments} />
+                    
                 </div>
             </div>
         );
